f1bc33dd73b5319a6602df8ade1eefe0fc0ff2b1
Ricardo Gomes <ipsiart@gmail.com>
2017-09-24 16:50:57 +0100

    exercise 2 completed

---
+++ 01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("CHAR: %d\n", sizeof(char));
+	printf("INT: %d\n", sizeof(int));
+	printf("LONG INT: %d\n", sizeof(long int));
+	printf("FLOAT: %d\n", sizeof(float));
+	printf("DOUBLE: %d\n", sizeof(double));
+}
