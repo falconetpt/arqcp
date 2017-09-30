aa970bc8498e995fe59d3594cc438b75139d0952
ipsiart@gmail.com <ipsiart@gmail.com>
2017-09-30 21:39:18 +0100

    exercise 1 added

--- PL01/01/ex01.c
+++ PL01/01/ex01.c
@@ -6,4 +6,5 @@ int main(void) {
 	float y = *xPtr + 3;
 	int vec[] = {10, 11, 12, 13};
 	print_elements(x, xPtr, y, vec);
+	return 0;
 }
--- PL01/01/print_data.c
+++ PL01/01/print_data.c
@@ -3,4 +3,10 @@
 void print_elements(int x, int *xPtr, float y, int *vec) {
 	printf("--Value:\n X: %d Y: %f\n", x, y);
 	printf("--Address of:\n X: %x XPTR: %x\n", &x, &xPtr);
+	printf("--Value Pointed by xPtr: %d\n", *xPtr);
+	printf("--Address of:\n vec: %x\n", &vec);
+	printf("--Values of vec:\n [0]: %d [1]: %d [2]: %d [3]: %d\n",
+			*(vec+0), *(vec+1), *(vec+2), *(vec+3));
+	printf("--Address of vec:\n [0]: %x [1]: %x [2]: %x [3]: %x\n",
+			(vec+0), (vec+1), (vec+2), (vec+3));
 }
