1fb4fb6e1bb2193cc7dd70ee95e4a1e15a3b4c9c
ipsiart@gmail.com <ipsiart@gmail.com>
2017-10-05 16:31:17 +0100

    added ex19

--- PL01/20/ex20.c
+++ PL01/20/ex20.c
@@ -3,16 +3,21 @@
 #include <string.h>
 
 int main(void) {
-	char *str = "12345124126";
-	char *word = "12";
+	char *str = "12A34512412a6";
+	char *word = "12a";
 	
 	int n = strlen(str);
 	char* arr[n];
 	int i;
 	
-	fill_all_words(str, word, addrs);
+	clear_arr(arr, n);
+	fill_all_words(str, word, arr);
 	
 	for(i = 0; i < n; i++) {
-		printf("%s\n", *(arr+i));
+		if(strlen(*(arr+i))>0) {
+			printf("%s\n", *(arr+i));
+		}
+		
 	}
+	return 0;
 }
--- PL01/20/find_all.c
+++ PL01/20/find_all.c
@@ -1,14 +1,24 @@
 #include "find_word.h"
+#include <string.h>
+
+void clear_arr(char** str, int n) {
+	int i;
+	for(i = 0; i < n; i++) {
+		*(str+i) = "";
+	}
+}
 
 void fill_all_words(char *str, char* word, char** addrs) {
 	int filled = 0;
 	int i =0;
+	char *tmp = str;
 	
 	while(*(str+i)) {
-		char *tmp = find_word(str, word, *(str+i));
+		tmp = find_word(str, word, (tmp));
 		
-		if(tmp!=NULL) {
+		if(tmp) {
 			*(addrs+filled) = tmp;
+			filled++;
 		} else {
 			return;
 		}
